f5448a9600f722f2e0e1a7b286079f35cdb2924a
tylercat719 <70349473+tylercat719@users.noreply.github.com>
2022-07-24 19:22:08 -0400

    Update lineage_hotdogg.mk

--- lineage_hotdogg.mk
+++ lineage_hotdogg.mk
@@ -12,7 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/hotdogg/device.mk)
 
 # Inherit from Oneplus Camera
-$(call inherit-product, vendor/oneplus/apps/hotdogg/config.mk)
+# DEAD
+#$(call inherit-product, vendor/oneplus/apps/hotdogg/config.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
